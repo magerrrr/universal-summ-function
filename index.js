449a57448ac85090382ff787c983377283eaaef5
Kirill Mager <kiruha24@mail.ru>
2019-12-11 18:08:15 +0300

    refactor: use arrow functions  + reduce method

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-sum = function(...args){
-  let result = 0;
-  for(let i = 0; i < args.length; i++){
-    result += args[i];
-  }
-  return result;
-  }
+const sum = (...args) => args.reduce((acc, value) => acc + value);
